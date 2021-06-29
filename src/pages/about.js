98748bb5928e29ed0843791aa47335a7a9eb55af
Alyssa Lindsey <alyssa.m.lindsey@gmail.com>
2021-06-29 12:59:40 -0600

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+      <Layout pageTitle='About Me'>
+        <p>Hi there! This is going to be a portfolio site for my photography. I'm using Gatsby to build it!</p>
+      </Layout> 
+  )
+}
+
+export default AboutPage
